43341e7b66ac6f15f657ebd9f925c6ec6d271ddd
sunlight9344 <sunlight9344@gmail.com>
2023-11-14 21:00:25 +0900

    2

--- src/main/java/com/poscodx/helloworld/HelloWorld.java
+++ src/main/java/com/poscodx/helloworld/HelloWorld.java
@@ -3,7 +3,8 @@ package com.poscodx.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("1");
+		System.out.println("2");
 	}
 
 }
